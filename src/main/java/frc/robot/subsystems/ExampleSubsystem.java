bc263aeb29272491b7eec324fd300afe63492a80
Justin Nguyen <justinnguyen4675@gmail.com>
2023-01-14 14:37:22 -0500

    limelight test

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -4,8 +4,12 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 
 public class ExampleSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
@@ -30,14 +34,30 @@ public class ExampleSubsystem extends SubsystemBase {
    *
    * @return value of some boolean subsystem state, such as a digital sensor.
    */
-  public boolean exampleCondition() {
+  public double getY() {
     // Query some boolean state, such as a digital sensor.
-    return false;
+    return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tiy").getDouble(0);
   }
-
+  double[] dv = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+  
+  //calculate distance
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+  getY();
+  double[] botpose =  NetworkTableInstance.getDefault().getTable("limelight").getEntry("camtran").getDoubleArray(dv);
+  
+    // This method will be called once per scheduler 
+  SmartDashboard.putNumber("BotX", Units.metersToInches(botpose[0]));
+  SmartDashboard.putNumber("Boty", Units.metersToInches(botpose[1]));
+  SmartDashboard.putNumber("BotZ", Units.metersToInches(botpose[2]));
+  SmartDashboard.putNumber("Bot1", Units.metersToInches(botpose[3]));
+  SmartDashboard.putNumber("Bot2", Units.metersToInches(botpose[4]));
+  SmartDashboard.putNumber("Bot3", Units.metersToInches(botpose[5]));  
+  SmartDashboard.putNumber("ID", NetworkTableInstance.getDefault().getTable("limelight").getEntry("tid").getDouble(0));
+  // SmartDashboard.putNumberArray("ROBOTLOCATON", NetworkTableInstance.getDefault().getTable("limelight").getEntry("botpose").getDoubleArray(dv));
+  SmartDashboard.putNumber("X", NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0));
+  SmartDashboard.putNumber("Y", NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0));
+  SmartDashboard.putNumber("DO YOU SEE ANYTHING ", NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0));
   }
 
   @Override
